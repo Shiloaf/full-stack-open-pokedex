735d298ebb79f113a0f9780f512021444f0d1293
Shiloaf <ninjacornomega@gmail.com>
2025-12-06 22:47:54 +0800

    reset deployment scheme to render

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: "https://Shiloaf.github.io/full-stack-open-pokedex",
+    // baseURL: "https://Shiloaf.github.io/full-stack-open-pokedex",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
