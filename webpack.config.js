b7352e23f7dbfe0033ab1e9ab25c5536a5e36afd
Shiloaf <ninjacornomega@gmail.com>
2025-12-06 13:42:14 +0800

    changed webpack config publicPath to repo

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/full-stack-open-pokedex'
   },
   module: {
     rules: [
